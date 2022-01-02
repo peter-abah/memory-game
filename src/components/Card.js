432d940b1296cd084b1c5762ffc7284681c07faa
Peter Abah <peterabah.ob@gmail.com>
2022-01-02 21:18:24 +0100

    Add styles for card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,9 @@
 const Card = (props) => {
   const { id, image, title, handleClick } = props;
   return (
-    <button data-id={id} onClick={handleClick}>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
+    <button className="card" data-id={id} onClick={handleClick}>
+      <img className="card__img" src={image} alt={title} />
+      <h3 className="card__title">{title}</h3>
     </button>
   );
 };
